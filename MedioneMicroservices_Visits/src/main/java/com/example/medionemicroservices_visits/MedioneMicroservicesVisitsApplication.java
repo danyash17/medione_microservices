c3aee9c809e565bb349a20c63757da2b69ec54ad
danyash17 <danikshkabara@tut.by>
2023-12-07 23:22:44 +0300

    Visits reconfigure to k8s srv disc

--- MedioneMicroservices_Visits/src/main/java/com/example/medionemicroservices_visits/MedioneMicroservicesVisitsApplication.java
+++ MedioneMicroservices_Visits/src/main/java/com/example/medionemicroservices_visits/MedioneMicroservicesVisitsApplication.java
@@ -2,11 +2,13 @@ package com.example.medionemicroservices_visits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @RefreshScope
+@EnableDiscoveryClient
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class MedioneMicroservicesVisitsApplication {
 
