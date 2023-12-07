d44ea1f07f190334d7e22f6a67271dae10a0e176
danyash17 <danikshkabara@tut.by>
2023-12-07 23:34:47 +0300

    Gateway reconfigure to k8s srv disc

--- MedioneGatewayServer/src/main/java/com/medione/gateway/medionegatewayserver/MedioneGatewayServerApplication.java
+++ MedioneGatewayServer/src/main/java/com/medione/gateway/medionegatewayserver/MedioneGatewayServerApplication.java
@@ -2,6 +2,7 @@ package com.medione.gateway.medionegatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -14,6 +15,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MedioneGatewayServerApplication {
 
     public static void main(String[] args) {
@@ -29,20 +31,20 @@ public class MedioneGatewayServerApplication {
                                         .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
                                         .circuitBreaker(config -> config.setName("doctorCB")
                                                 .setFallbackUri("forward:/error")))
-                                .uri("lb://MMDOCTOR"))
+                                .uri("http://mmdoctor:8081"))
                 .route(p ->
                         p.path("/medione/patient/**")
                                 .filters(f -> f.rewritePath("/medione/(?<remaining>.*)", "/medioneapi/${remaining}")
                                         .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
                                         .retry(retryConfig -> retryConfig.setRetries(3).setMethods(HttpMethod.GET)
                                                 .setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true)))
-                                .uri("lb://MMPATIENT"))
+                                .uri("http://mmpatient:8082"))
                 .route(p ->
                         p.path("/medione/visits/**")
                                 .filters(f -> f.rewritePath("/medione/(?<remaining>.*)", "/medioneapi/${remaining}")
                                         .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
                                         .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter()).setKeyResolver(keyResolver())))
-                                .uri("lb://MMVISITS"))
+                                .uri("http://mmvisits:8083"))
                 .build();
     }
 
